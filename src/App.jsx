369f764138228910b8cc9d568b6f4c21b9a32d0f
Michal <michal@drontrzebownisko.pl>
2024-02-02 11:39:59 +0100

    Structured file organization, updated favicon, added font, and refined color scheme.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      {/* Tutaj będziemy dodawać kolejne elementy naszej gry, jak plansza czy przyciski do sterowania */}
+    </div>
+  );
+}
+
+export default App;
