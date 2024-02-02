c59627f0e6f7b505cfd612a0d4f4b539804ec565
Michal <michal@drontrzebownisko.pl>
2024-02-02 16:09:25 +0100

    Added player name editing feature and Enter key listener for saving.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
+
 function Player({ playerName, playerSymbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const [editableName, setEditableName] = useState(playerName);
+
+  function handleEditClick() {
+    setIsEditing((prev) => !prev);
+    setIsActive((prev) => !prev);
+  }
+
+  let player = <span className="player-name">{editableName}</span>;
+  if (isEditing) {
+    player = (
+      <input
+        type="text"
+        required
+        value={editableName}
+        onChange={(e) => setEditableName(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") handleEditClick();
+        }}
+      />
+    );
+  }
+
   return (
     <span className="player">
-      <span className="player-name">{playerName}</span>
+      {player}
       <span className="player-symbol">{playerSymbol}</span>
-      <button>Edytuj</button>
+      <button className={isActive ? "active" : ""} onClick={handleEditClick}>
+        {isEditing ? "Zapisz" : "Edytuj"}
+      </button>
     </span>
   );
 }
